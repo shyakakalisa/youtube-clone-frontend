7366f59f13d31c22975a6594ba8fb9eecd74d730
shyakakalisa <shyakakalisa@gmail.com>
2026-01-28 20:48:44 -0800

    Add mock video data with real thumbnails and video IDs
    Added thumbnail URLs for each video in the mock data.

--- js/data.js
+++ js/data.js
@@ -1,23 +1,26 @@
-// Mock video data with real YouTube video IDs
 const videos = [
   {
     id: "dQw4w9WgXcQ",
     title: "Frontend Developer Roadmap",
-    channel: "Dev Academy"
+    channel: "Dev Academy",
+    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/hqdefault.jpg"
   },
   {
     id: "3PHXvlpOkf4",
     title: "HTML CSS JavaScript Full Course",
-    channel: "Code With Harry"
+    channel: "Code With Harry",
+    thumbnail: "https://img.youtube.com/vi/3PHXvlpOkf4/hqdefault.jpg"
   },
   {
     id: "PkZNo7MFNFg",
     title: "JavaScript Crash Course",
-    channel: "Traversy Media"
+    channel: "Traversy Media",
+    thumbnail: "https://img.youtube.com/vi/PkZNo7MFNFg/hqdefault.jpg"
   },
   {
     id: "W6NZfCO5SIk",
     title: "JavaScript Tutorial for Beginners",
-    channel: "Programming with Mosh"
+    channel: "Programming with Mosh",
+    thumbnail: "https://img.youtube.com/vi/W6NZfCO5SIk/hqdefault.jpg"
   }
 ];
